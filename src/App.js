29241416ade533e669e763838865b4c614a633de
anshuul <anshultated1999@gmail.com>
2020-10-10 22:54:29 +0530

    Made changes to UI and added more details of current weather

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Axios from "axios";
+import DisplayWeather from "./Components/DisplayWeather/displayWeather";
 
 const api = {
   key: "850d3cf2a23c2ac0ba9e20dbdf545e68",
@@ -13,49 +14,36 @@ class App extends Component {
     inputAdded: "",
   };
 
-  dateBuilder = (d) => {
-    let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+  /*   componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      Axios.get(
+        `${api.base}onecall?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&APPID=${api.key}`
+      )
+        .then((response) => {
+          console.log(response);
+          this.displayWeatherInfo(response);
+        })
+        .catch((error) => console.log(error));
+    });
+  }
 
-    return `${day} ${date}, ${month} ${year}`;
-  };
+  displayWeatherInfo = (response) => (
+    <DisplayWeather
+      name={response.data.name}
+      country={response.data.sys.country}
+      temp={response.data.main.temp}
+      icon={response.data.weather[0].icon}
+      description={response.data.weather[0].description}
+      data={response.data.weather[0].main}
+    />
+  ); */
 
   locationAdded = (event) => {
     const newLocation = event.target.value;
 
-    this.setState(
-      {
-        inputAdded: newLocation,
-      },
-      () => {
-        console.log(this.state.inputAdded);
-      }
-    );
+    this.setState({
+      inputAdded: newLocation,
+    });
   };
 
   findLocationWeather = (e) => {
@@ -67,7 +55,7 @@ class App extends Component {
           console.log(response);
           this.setWeather(response);
         })
-        .catch((error) => alert(`City does not exist`));
+        .catch((error) => alert(error));
       this.setState({
         inputAdded: "",
       });
@@ -76,20 +64,20 @@ class App extends Component {
 
   setWeather = (result) => {
     this.setState({ weather: result });
-    console.log("Weather Received", this.state.weather.data.name);
+    console.log("Weather Received for", this.state.weather.data.name);
   };
+
+  timeConverter(timestamp) {
+    const date = new Date(timestamp * 1000);
+    const hours = date.getHours();
+    const mins = date.getMinutes();
+    const finalTime = hours + ":" + mins;
+    return finalTime;
+  }
 
   render() {
     return (
-      <div
-        className={
-          this.state.weather != ""
-            ? this.state.weather.data.main.temp > 16
-              ? "App Warm"
-              : "App"
-            : "App"
-        }
-      >
+      <div className={"App"}>
         <main>
           <div className="search-box">
             <input
@@ -101,24 +89,23 @@ class App extends Component {
               value={this.state.inputAdded}
             />
           </div>
-          {this.state.weather != "" ? (
-            <div>
-              <div className="location-box">
-                <div className="location">
-                  {this.state.weather.data.name},{" "}
-                  {this.state.weather.data.sys.country}
-                </div>
-                <div className="date">{this.dateBuilder(new Date())}</div>
-              </div>
-              <div className="weather-box">
-                <div className="temp">
-                  {Math.round(this.state.weather.data.main.temp)}°C
-                </div>
-                <div className="weather">
-                  {this.state.weather.data.weather[0].main}
-                </div>
-              </div>
-            </div>
+          {this.state.weather !== "" ? (
+            <DisplayWeather
+              name={this.state.weather.data.name}
+              country={this.state.weather.data.sys.country}
+              temp={this.state.weather.data.main.temp}
+              icon={this.state.weather.data.weather[0].main}
+              description={this.state.weather.data.weather[0].description}
+              data={this.state.weather.data.weather[0].main}
+              //maxtemp={this.state.weather.data.main.temp_max}
+              //mintemp={this.state.weather.data.main.temp_min}
+              feelslike={this.state.weather.data.main.feels_like}
+              humidity={this.state.weather.data.main.humidity}
+              sunrise={this.timeConverter(this.state.weather.data.sys.sunrise)}
+              sunset={this.timeConverter(this.state.weather.data.sys.sunset)}
+              wind={this.state.weather.data.wind.speed}
+              visibility={this.state.weather.data.visibility}
+            />
           ) : (
             ""
           )}
---
+++ src/Components/DisplayWeather/displayWeather.js
@@ -0,0 +1,119 @@
+import React from "react";
+import "./displayWeather.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCloud,
+  faBolt,
+  faCloudRain,
+  faCloudShowersHeavy,
+  faSnowflake,
+  faSun,
+  faSmog,
+} from "@fortawesome/free-solid-svg-icons";
+
+const displayWeather = (props) => {
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date}, ${month} ${year}`;
+  };
+
+  let weatherIcon = null;
+
+  if (props.icon === "Thunderstorm") {
+    weatherIcon = <FontAwesomeIcon icon={faBolt} />;
+  } else if (props.icon === "Drizzle") {
+    weatherIcon = <FontAwesomeIcon icon={faCloudRain} />;
+  } else if (props.icon === "Rain") {
+    weatherIcon = <FontAwesomeIcon icon={faCloudShowersHeavy} />;
+  } else if (props.icon === "Snow") {
+    weatherIcon = <FontAwesomeIcon icon={faSnowflake} />;
+  } else if (props.icon === "Clear") {
+    weatherIcon = <FontAwesomeIcon icon={faSun} />;
+  } else if (props.icon === "Clouds") {
+    weatherIcon = <FontAwesomeIcon icon={faCloud} />;
+  } else {
+    weatherIcon = <FontAwesomeIcon icon={faSmog} />;
+  }
+  return (
+    <div className="display">
+      <div className="div1">
+        <div className="location-box">
+          <div className="location">
+            {props.name},{props.country}
+          </div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">{Math.round(props.temp)}°C</div>
+          <div className="weather">
+            <div className="city-icon">{weatherIcon}</div>
+            <p> {props.data} </p>
+          </div>
+        </div>
+      </div>
+      <div className="div2">
+        <div className="forecast">
+          <div className="firstrow">
+            <div className="details">
+              <div className="data">{Math.round(props.feelslike)}°C</div>
+              <p>Feels like</p>
+            </div>
+            <div className="details">
+              <div className="data">{props.sunrise} </div>
+              <p>Sunrise</p>
+            </div>
+            <div className="details">
+              <div className="data">{props.wind}mph</div>
+              <p>Wind</p>
+            </div>
+          </div>
+          <div className="secondrow">
+            <div className="details">
+              <div className="data">{props.humidity}</div>
+              <p>Humidity</p>
+            </div>
+            <div className="details">
+              <div className="data">{props.sunset} </div>
+              <p>Sunrise</p>
+            </div>
+            <div className="details">
+              <div className="data">
+                {(props.visibility / 1000).toFixed(1)}Km
+              </div>
+              <p>Visibilty</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default displayWeather;
